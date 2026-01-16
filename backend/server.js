88a7acb579747dd4666bc95b85f78717e4df3aab
RiteshRana25 <160099737+RiteshRana25@users.noreply.github.com>
2026-01-16 15:45:43 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,20 +3,18 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
+const leadRoutes = require("./routes/Leads");
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("Connected to MongoDB"))
-  .catch(err => console.error(err));
-
-const leadRoutes = require("./routes/Leads");
 app.use("/api/leads", leadRoutes);
-
 app.get("/", (req, res) => {
   res.send("API is working!");
 });
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.error(err));
 
 module.exports = app;
