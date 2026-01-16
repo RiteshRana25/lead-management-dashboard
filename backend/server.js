08cdd4670dff6352fe46f244e4ed9c4ed929c300
RiteshRana25 <160099737+RiteshRana25@users.noreply.github.com>
2026-01-16 15:27:23 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
-
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB"))
@@ -17,4 +15,8 @@ mongoose
 const leadRoutes = require("./routes/Leads");
 app.use("/api/leads", leadRoutes);
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.get("/", (req, res) => {
+  res.send("API is working!");
+});
+
+module.exports = app;
